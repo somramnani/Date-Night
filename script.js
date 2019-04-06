7823ced7b8aed75824a6b1c766c10646d7977a40
Brent Abruzese <brent.abruzese@gmail.com>
2019-04-06 12:10:23 -0400

    yelp api functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+$("<div>").class("card");
+$("<div>").class("card-img-top").attr("src", url);
+$("<div>").class("card-header");
+$("<div>").class("card-body");
+$("<div>").class("card-footer");
+

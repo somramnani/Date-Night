45b6854834b478226cf1398f09caa8aae3be3775
Brent Abruzese <brent.abruzese@gmail.com>
2019-04-06 12:16:06 -0400

    Delete script.js
    bad file - not a part of the project

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-$("<div>").class("card");
-$("<div>").class("card-img-top").attr("src", url);
-$("<div>").class("card-header");
-$("<div>").class("card-body");
-$("<div>").class("card-footer");
-

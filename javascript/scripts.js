8db29c3092e21eeb00ae81412bdeb478991f4fda
Som Ramnani <som.ramnani@gmail.com>
2019-04-07 17:44:02 -0500

    Cleaned up the HTML & JS code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,10 +6,15 @@ console.warn('Project One JS Initialized');
 //__________________________________________________________
 //GLOBAL VARIABLES
 //__________________________________________________________
-	var gps =  $(".gps").val();
+  
+  // Location Input
+  var gps =  $(".gps").val();
 
-
-	
+  
+  var lat;
+  var lng;
+  var address = "54 Tracy Drive, Manalapan, NJ 07726";
+  
   
   //________________________________________________________
   //Price Button Variables
@@ -24,7 +29,9 @@ console.warn('Project One JS Initialized');
     // Price button input field
     var priceEntry = $(".price_entry");
   //________________________________________________________
+
 //__________________________________________________________
+
 
 //__________________________________________________________
 //BUTTON FUNCTIONS
@@ -65,10 +72,8 @@ console.warn('Project One JS Initialized');
     
     // Activity button input field
 		var activityEntry = $(".activities_entry");
-
-
-		
   //________________________________________________________
+
 //__________________________________________________________
 
 
@@ -123,67 +128,53 @@ console.warn('Project One JS Initialized');
     $(".activities5").on("click", function(){
       activityEntry.attr("value", activities5);
 		})
-		
+    
+    // The input value of activities`
 		var activityEntryValue = $(".activities_entry").val();
-
-
   //______________________________________________________
 
-//________________________________________________________
+  //________________________________________________________
+  //Price Button On-Click Functions
+  //_______________________________________________________
+    $("#submit").on("click", function() {
+      var addressInput = $("#location").val().trim();
+      var yelpAPIKey = "V3BqWR13gf4DYXvRewAG0jVi7K7Xy-yLxjzRTFA29eZPdSiS1aFqyxVXq1PNP2e_m4Xl8cDdypAroctE4HFsP0ZY7_oGX0Xmvm7kZ6_WtTMAqCx2k_qljY0j3qymXHYx"
+      var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + addressInput;
 
-/* eslint-enable */
-
-// https://api.yelp.com/v3/businesses/{id}
-// rating, price, url, photo/image-url 
-
-
-var lat;
-var lng;
-
-var address = "54 Tracy Drive, Manalapan, NJ 07726";
-
-/* eslint-enable */
-
-// rating, price, url, photo/image-url
-
-$("#submit").on("click", function() {
-  var addressInput = $("#location").val().trim();
-  var yelpAPIKey = "V3BqWR13gf4DYXvRewAG0jVi7K7Xy-yLxjzRTFA29eZPdSiS1aFqyxVXq1PNP2e_m4Xl8cDdypAroctE4HFsP0ZY7_oGX0Xmvm7kZ6_WtTMAqCx2k_qljY0j3qymXHYx"
-  var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + addressInput;
-
-  var eventbriteAPIKey = "QHHRQKYP5TZBK3NVPHD2";
-  var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + addressInput + "&token=QHHRQKYP5TZBK3NVPHD2";
-  var OAuthKey = "QHHRQKYP5TZBK3NVPHD2";
-  var eventSearchURL = "https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=" + addressInput;
-  
-
-    $.ajax({
-      url: yelpURL,
-      headers: {
-      	'Authorization': 'Bearer ' + yelpAPIKey,
-            },
+      var eventbriteAPIKey = "QHHRQKYP5TZBK3NVPHD2";
+      var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + addressInput + "&token=QHHRQKYP5TZBK3NVPHD2";
+      var OAuthKey = "QHHRQKYP5TZBK3NVPHD2";
+      var eventSearchURL = "https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=" + addressInput;
+        
+      $.ajax({
+        url: yelpURL,
+        headers: {
+          'Authorization': 'Bearer ' + yelpAPIKey,    
+        },    
         method: "GET",
-        dataType: 'json' 
-    }).then(function(response) {
-        console.warn("<--------yelp results----->");
-        console.log(response)
-        var resultArray = [];
-        for(i = 0; i < response.businesses.length; i++);
-        resultArray.push([i]);
+          dataType: 'json' 
 
-    })
-
-    $.ajax({
+        }).then(function(response) {
+            console.warn("<--------yelp results----->");
+            console.log(response)
+            var resultArray = [];
+            for(i = 0; i < response.businesses.length; i++){        
+              resultArray.push([i]);
+            }
+          })
+      
+      $.ajax({
         url: eventbriteURL,
         method: "GET"
-    }).then(function(response) {
-        console.warn("<------eventbrite results------->");
-        console.log(response);
-    })
-
-
+        }).then(function(response) {
+            console.warn("<------eventbrite results------->");
+            console.log(response);
+        })
         console.log(resultArray);
-  })
+    })
+  //_______________________________________________________
+
+//________________________________________________________
 
 
 

0be847e3788b5ed63ce9231c7237af53ca74db3c
Som Ramnani <som.ramnani@gmail.com>
2019-04-09 20:15:50 -0400

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -141,62 +141,14 @@ console.warn('Project One JS Initialized');
         var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + addressInput + "&token=QHHRQKYP5TZBK3NVPHD2";
         var OAuthKey = "QHHRQKYP5TZBK3NVPHD2";
         var eventSearchURL = "https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=" + addressInput;
+      	var fandangoApiKey = "ncpps2jdywggp62dwfegnruz";
+      	var fandangoSecretKey = "BmyAFhX3EV";
+      	var fandangoURL = "";
+      	var fandangoSearchURL = "";
       //______________________________________________________
 
 
-      var addressInput = $("#location").val().trim();
-      var yelpAPIKey = "V3BqWR13gf4DYXvRewAG0jVi7K7Xy-yLxjzRTFA29eZPdSiS1aFqyxVXq1PNP2e_m4Xl8cDdypAroctE4HFsP0ZY7_oGX0Xmvm7kZ6_WtTMAqCx2k_qljY0j3qymXHYx"
-      var yelpURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + addressInput + "&limit=6";
-
-      var eventbriteAPIKey = "QHHRQKYP5TZBK3NVPHD2";
-      var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + addressInput + "&token=QHHRQKYP5TZBK3NVPHD2";
-      var OAuthKey = "QHHRQKYP5TZBK3NVPHD2";
-      var eventSearchURL = "https://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search?location.address=" + addressInput;
-        
-      var fandangoApiKey = "ncpps2jdywggp62dwfegnruz";
-      var fandangoSecretKey = "BmyAFhX3EV";
-      var fandangoURL = "";
-      var fandangoSearchURL = "";
-
-      $.ajax({
-        url: yelpURL,
-        headers: {
-          'Authorization': 'Bearer ' + yelpAPIKey,    
-        },    
-        method: "GET",
-          dataType: 'json' 
-
-        }).then(function(response) {
-           // var resultArray = [];
-            console.warn("<--------yelp results----->");
-            console.log(response);
-
-            for(i = 0; i < response.businesses.length; i++){   
-              // Restauraunt Results Row
-              var newRow = $("<tr>");
-              var restaurauntDiv  = $("#restaurant-results");
-
-              // resultArray.push([i]);
-
-              // API information stored into variables
-              var price = response.businesses[i].price;
-
-              var image = response.businesses[i].image_url;
-              var webURL = response.businesses[i].url;
-
-              var name = response.businesses[i].name;
-
-              // If the user clicks price, and restaraunts display the specific budget and Restaurants.
-              if (priceEntryValue === price  && activityEntryValue === "Restaurants" ) {
-                console.log(response.businesses[i]);
-                let restBtn = $("<a>").addClass("resultBtn m-2").html(name).attr("href", webURL).attr("data-toggle", "popover").attr("title", "information").attr("data-content", "information goes here");
-                restaurauntDiv.append(restBtn);
-
-              }
-          }                  
-        })
-
-      
+ 
       //______________________________________________________
       //API'S AJAX REQUEST
       //______________________________________________________
